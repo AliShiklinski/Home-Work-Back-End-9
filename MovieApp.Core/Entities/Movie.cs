95c110078d16a9a7b931db604103284587ce45ff
AliShiklinski <169164888+AliShiklinski@users.noreply.github.com>
2024-09-14 22:47:19 +0400

    added

---
+++ MovieApp.Core/Entities/Movie.cs
@@ -0,0 +1,10 @@
+﻿namespace MovieApp.Core.Entities
+{
+    public class Movie: BaseEntity
+    {
+        public string Title { get; set; }
+        public string Desc { get; set; }
+        public int GenreId { get; set; }
+        public Genre Genre { get; set; }
+    }
+}
